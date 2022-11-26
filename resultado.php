a58254b779c7554bcc080aa7aa1da2aba0d4c1ff
ZzDIGOzZ <jorge310c@gmail.com>
2022-11-26 20:04:31 +0000

    Update resultado.php

--- resultado.php
+++ resultado.php
@@ -1,89 +1,22 @@
 <?php
 	
 
-	//esta variavel vai armazenar o valor da pergunta 1 
-	$quest01 = $_GET['quest01'];
-
-	//esta variavel vai armazenar o valor da pergunta 2
-	$quest02 = $_GET['quest02'];
-
-	//esta variavel vai armazenar o valor da pergunta 3
-	$quest03 = $_GET['quest03'];
-
-
-	//este switch vai verificar qual foi a opção selecionada da primeira pregunta do outro script e cada case vai ter um valor equivalente a cada pergunta que foi defenido no atributo value do input da opção
-	switch ($quest01) {
-		case 1:
-			echo $quest01;
-			break;
-
-		case 2:
-			echo $quest01;
-			break;
-
-		case 3:
-			echo $quest01;
-			break;
-
-		case 4:
-			echo $quest01;
-			break;
-		
-	}
+	//estas variaveis vão armazenar o valor das perguntas de 1 a 10
+	$quest01 = $_POST['quest01'];
+	$quest02 = $_POST['quest02'];
+	//$quest03 = $_POST['quest03'];
+	//$quest04 = $_POST['quest04'];
+	//$quest05 = $_POST['quest05'];
+	//$quest06 = $_POST['quest06'];
+	//$quest07 = $_POST['quest07'];
+	//$quest08 = $_POST['quest08'];
+	//$quest09 = $_POST['quest09'];
+	//$quest10 = $_POST['quest10'];
 
 
-	echo "<br>";
+	//variavel que vai armazenar o valor do resultado
+	$quests_result = $quest01 + $quest02 /*+ $quest03 + $quest04 + $quest05 + $quest06 + $quest07 + $quest08 + $quest09 + $quest10*/;
 
-	//este switch vai verificar qual foi a opção selecionada da segunda pregunta do outro script e cada case vai ter um valor equivalente a cada pergunta que foi defenido no atributo value do input da opção
-	switch ($quest02) {
-		case 1:
-			echo $quest02;
-			break;
-
-		case 2:
-			echo $quest02;
-			break;
-
-		case 3:
-			echo $quest02;
-			break;
-
-		case 4:
-			echo $quest02;
-			break;
-		
-	}
-
-	echo "<br>";
-
-	switch ($quest03) {
-		case 1:
-			echo $quest03;
-			break;
-
-		case 2:
-			echo $quest03;
-			break;
-
-		case 3:
-			echo $quest03;
-			break;
-
-		case 4:
-			echo $quest03;
-			break;
-		
-	}
-
-	echo "<br>";
-
-
-
-	//esta variavel vai calcular os pontos que se obteve em cada pergunta
-	$resultado = $quest01 + $quest02 + $quest03;
-
-	//vai printar os pontos que se obteve 
-	echo "o resultado do teu teste é $resultado";
 
 
 
