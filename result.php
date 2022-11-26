4b4d4813e3a8ab152b581bc66e98cf7e0bf41ee4
ZzDIGOzZ <jorge310c@gmail.com>
2022-11-26 20:03:33 +0000

    Create result.php

---
+++ result.php
@@ -0,0 +1,27 @@
+<?php
+	
+
+	//estas variaveis vão armazenar o valor das perguntas de 1 a 10
+	$quest01 = $_POST['quest01'];
+	$quest02 = $_POST['quest02'];
+	//$quest03 = $_POST['quest03'];
+	//$quest04 = $_POST['quest04'];
+	//$quest05 = $_POST['quest05'];
+	//$quest06 = $_POST['quest06'];
+	//$quest07 = $_POST['quest07'];
+	//$quest08 = $_POST['quest08'];
+	//$quest09 = $_POST['quest09'];
+	//$quest10 = $_POST['quest10'];
+
+
+	//variavel que vai armazenar o valor do resultado
+	$quests_result = $quest01 + $quest02 /*+ $quest03 + $quest04 + $quest05 + $quest06 + $quest07 + $quest08 + $quest09 + $quest10*/;
+
+
+
+
+
+
+
+?>
+
