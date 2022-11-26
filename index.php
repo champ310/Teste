48ce8d33f55188e317e77732f463b99e4a62a475
ZzDIGOzZ <jorge310c@gmail.com>
2022-11-26 18:51:35 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,62 @@
+<html>
+
+	<head>
+		
+		<meta charset="utf-8">
+
+		<title>teste</title>
+	
+	</head>
+
+
+	<body>
+		
+		<form method="GET" action="resultado.php">
+
+			Escolhe uma opção: <br><br>
+
+			<input type="radio" name="quest01" value="1" checked> opção 01 <br>
+			
+			<input type="radio" name="quest01" value="2"> opção 02 <br>
+
+			<input type="radio" name="quest01" value="3"> opção 03 <br>
+
+			<input type="radio" name="quest01" value="4"> opção 04 <br><br>
+
+
+
+			Escolhe uma opção: <br><br>
+
+			<input type="radio" name="quest02" value="1" checked> opção 01 <br>
+			
+			<input type="radio" name="quest02" value="2"> opção 02  <br>
+
+			<input type="radio" name="quest02" value="3"> opção 03 <br>
+
+			<input type="radio" name="quest02" value="4"> opção 04 <br><br>
+
+
+
+
+
+			Escolhe uma opção: <br><br>
+
+			<input type="radio" name="quest03" value="1" checked> opção 01 <br>
+			
+			<input type="radio" name="quest03" value="2"> opção 02  <br>
+
+			<input type="radio" name="quest03" value="3"> opção 03 <br>
+
+			<input type="radio" name="quest03" value="4"> opção 04 <br><br>
+
+
+			<input type="submit" name="Enviar">
+
+
+		</form>
+
+
+	</body>
+
+
+</html>
