dfb7a2260f492d067107ed580e722f350a6f155a
champ310 <103106069+champ310@users.noreply.github.com>
2023-01-14 18:29:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,47 +11,86 @@
 
 	<body>
 		
-		<form method="GET" action="resultado.php">
+		<form method="POST" action="resultado.php">
+			<!-- #1 -->
+			Wählen Sie eine Option: <br>"Moralische Normen unterscheiden sich je nach Kultur und historischer Epoche.
+			 Sie können auch in Frage gestellt und entlassen werden". Das bedeutet, dass:<br>
 
-			Escolhe uma opção: <br><br>
-
-			<input type="radio" name="quest01" value="1" checked> opção 01 <br>
+			<input type="radio" name="quest01" value="1"> a) Wir können nicht über die moralischen Normen nachdenken, die uns auferlegt werden; <br>
 			
-			<input type="radio" name="quest01" value="2"> opção 02 <br>
+			<input type="radio" name="quest01" value="2"> b) Wir müssen den moralischen Normen zustimmen, weil sie die Normen unserer Kultur sind; <br>
 
-			<input type="radio" name="quest01" value="3"> opção 03 <br>
+			<input type="radio" name="quest01" value="0"> c) Die Moral ist ein Wertesystem, an dem sich die Menschen in ihrem Verhalten orientieren, und unterliegt daher je nach Land und historischem Moment, in dem sich die Menschen befinden, einem Wandel. <br>
 
-			<input type="radio" name="quest01" value="4"> opção 04 <br><br>
+			<input type="radio" name="quest01" value="4"> d) Wir handeln nicht "moralisch", wenn wir die von der Gesellschaft aufgestellten Regeln befolgen.<br>
 
 
 
-			Escolhe uma opção: <br><br>
+			<!-- #2 -->
 
-			<input type="radio" name="quest02" value="1" checked> opção 01 <br>
+			Wählen sie eine Option: <br>Wie können wir zwischen "Moral" und "Ethik" unterscheiden?<br>
+
+			<input type="radio" name="quest02" value="1"> a) Wir können nicht differenzieren, es sind synonyme Begriffe. <br>
 			
-			<input type="radio" name="quest02" value="2"> opção 02  <br>
+			<input type="radio" name="quest02" value="0"> b) Moral ist eine Reihe von Werten, und Ethik ist die Reflexion über diese Werte. <br>
 
-			<input type="radio" name="quest02" value="3"> opção 03 <br>
+			<input type="radio" name="quest02" value="3"> c) Moral ist die Praxis der Ethik in unserem täglichen Leben.<br>
 
-			<input type="radio" name="quest02" value="4"> opção 04 <br><br>
-
-
+			<input type="radio" name="quest02" value="4"> d) Moral ist ein Synonym für "angewandte Ethik".<br><br>
 
 
 
-			Escolhe uma opção: <br><br>
+			<!-- #3 -->
 
-			<input type="radio" name="quest03" value="1" checked> opção 01 <br>
+			Wählen sie eine Option: <br>"... Mit anderen Worten: Es gibt keinen Determinismus, der Mensch ist frei, der Mensch ist Freiheit. 
+			[...] Wir finden vor uns keine Werte oder Aufforderungen, die unser Verhalten legitimieren. Wir haben also weder hinter uns noch vor uns, im leuchtenden Bereich der Werte, Rechtfertigungen oder Entschuldigungen. Wir sind allein und haben keine Ausreden. 
+			Das will ich damit ausdrücken, dass der Mensch dazu verurteilt ist, frei zu sein. Verurteilt, weil er sich nicht selbst erschaffen hat, und doch frei, weil er, einmal in die Welt geworfen, für alles, was er tut, verantwortlich ist". 
+			Jean-Paul Sartre<br>
+
+			<input type="radio" name="quest03" value="1"> a) Es gibt keine Beziehung zwischen Gesetzen und Sitten, weil es die Menschen sind, die die Gesetze machen, die ihnen nützen. <br>
 			
-			<input type="radio" name="quest03" value="2"> opção 02  <br>
+			<input type="radio" name="quest03" value="0"> b) Um besondere menschliche Interessen zu begrenzen, muss es Gesetze geben, die allgemeine Interessen bevorzugen.<br>
 
-			<input type="radio" name="quest03" value="3"> opção 03 <br>
+			<input type="radio" name="quest03" value="3"> c) Die Menschen verfolgen ihre Interessen, und das bedeutet nicht, dass sie böse sind;<br>
 
-			<input type="radio" name="quest03" value="4"> opção 04 <br><br>
+			<input type="radio" name="quest03" value="4"> d) Es gibt eine Beziehung zwischen Gesetzen und Sitten, denn Gesetze ermöglichen oder verhindern, dass Menschen Fehler machen. <br>
+
+
+
+			<!-- #4 -->
+
+			Wählen sie eine Option: <br>"Die Menschen sind nicht böse, sondern ihren Interessen unterworfen.... 
+			Daher ist nicht die Schlechtigkeit der Menschen zu beklagen, sondern die Unwissenheit der Gesetzgeber, die stets das Partikularinteresse dem Allgemeininteresse entgegensetzen. 
+			[...] Bis heute ist es den schönsten moralischen Maximen nicht gelungen, eine Änderung der Sitten der Völker herbeizuführen. Was ist die Ursache? Es ist so, dass die Laster eines Volkes, wenn ich zu sprechen wage, auf dem Grund seiner Gesetzgebung verborgen sind." 
+			Helvetius
+
+			Welches sind die Hauptgedanken, die in dem oben genannten Fragment enthalten sind?<br>
+
+
+			<input type="radio" name="quest04" value="1"> a) Während Sartre von einem Determinismus ausgeht, vertritt Marx die Auffassung, dass der Mensch unabhängig von den Umständen frei ist. <br>
+			
+			<input type="radio" name="quest04" value="0"> b) Sartre vertritt die Auffassung, dass es keinen Determinismus gibt, und Marx legt einen Mittelweg zwischen Determinismus und totaler menschlicher Freiheit fest;  <br>
+
+			<input type="radio" name="quest04" value="0"> c) Wenn Sartre sagt: "Der Mensch ist dazu verurteilt, frei zu sein", sagt er dasselbe wie Marx, wenn er behauptet, dass "die Menschen ihre eigene Geschichte machen, aber sie machen sie nicht, wie sie wollen". <br>
+
+			<input type="radio" name="quest04" value="0"> d) Sartre sagt, dass der Mensch durch seine eigene Existenz begrenzt ist, während Marx sagt, dass der Mensch durch die historischen Bedingungen begrenzt ist. <br><br>
+
+			
+			<!-- #5 -->
+
+			Das ethisch-moralische Subjekt ist nur derjenige, der die folgenden Anforderungen erfüllt:
+
+			<input type="radio" name="quest04" value="1"> a) sich ihrer selbst bewusst sein, aber nicht die Existenz anderer als ethische Subjekte anerkennen müssen, die ihnen gleichgestellt sind.
+
+			<input type="radio" name="quest04" value="0"> b) wissen, was sie tun, kennen die Gründe und Ziele ihrer Handlungen, die Bedeutung ihrer Absichten und Haltungen und das Wesen der moralischen Werte.
+
+			<input type="radio" name="quest04" value="3"> c) seine Impulse, Neigungen und Leidenschaften nicht innerlich zu kontrollieren braucht, sondern sie frei fließen lässt
+
+			<input type="radio" name="quest04" value="4"> e) verantwortungsbewusst zu sein, sich aber nicht als Urheber der eigenen Handlung zu erkennen oder deren Auswirkungen und Folgen für sich und andere zu bewerten
+
 
 
 			<input type="submit" name="Enviar">
-
 
 		</form>
 
